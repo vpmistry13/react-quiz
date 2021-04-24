f5d3bc322d5fb62fc77ec8c2489124d1c8a97ca1
Vishal Mistry <vishalmistri93@gmail.com>
2021-04-24 16:20:32 +0530

    Login setup

---
+++ src/HomePage/HomePage.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { userActions } from '../_actions';
+
+class HomePage extends React.Component {
+    componentDidMount() {
+        this.props.getUsers();
+    }
+
+    handleDeleteUser(id) {
+        return (e) => this.props.deleteUser(id);
+    }
+
+    render() {
+        const { user, users } = this.props;
+        return (
+            <section className="container">
+            <div className="row">
+                <h1>Hi {user.firstName}!</h1>
+                <p>You're logged in with React!!</p> 
+                <p>
+                    <Link to="/login">Logout</Link>
+                </p>
+            </div>
+            </section>
+        );
+    }
+}
+
+function mapState(state) {
+    const { users, authentication } = state;
+    const { user } = authentication;
+    return { user, users };
+}
+
+const actionCreators = {
+    getUsers: userActions.getAll,
+    deleteUser: userActions.delete
+}
+
+const connectedHomePage = connect(mapState, actionCreators)(HomePage);
+export { connectedHomePage as HomePage };
---
+++ src/_services/user.service.js
@@ -0,0 +1,132 @@
+import config from 'config';
+import { authHeader } from '../_helpers';
+export const userService = {
+    login,
+    logout,
+    register,
+    getAll,
+    getById,
+    update,
+    delete: _delete
+};
+
+function login(username, password) {
+    const requestOptions = { 
+        headers: { 'Content-Type': 'application/json' },
+        // body: JSON.stringify({ username, password })
+    };
+
+    // return fetch(`${config.apiUrl}/users/authenticate`, requestOptions)
+    return fetch('../user_db/data.json', requestOptions)
+        .then(res => handleResponseLogin(res,username,password))
+        .then(user => {
+            console.log(user)
+            // store user details and jwt token in local storage to keep user logged in between page refreshes
+            localStorage.setItem('user', JSON.stringify(user));
+
+            return user;
+        });
+}
+
+function logout() {
+    // remove user from local storage to log user out
+    localStorage.removeItem('user');
+}
+
+function getAll() {
+    const requestOptions = {
+        method: 'GET',
+        headers: authHeader()
+    };
+
+    return fetch(`${config.apiUrl}/users`, requestOptions).then(handleResponse);
+}
+
+function getById(id) {
+    const requestOptions = {
+        method: 'GET',
+        headers: authHeader()
+    };
+
+    return fetch(`${config.apiUrl}/users/${id}`, requestOptions).then(handleResponse);
+}
+
+function register(user) {
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(user)
+    };
+
+    return fetch(`${config.apiUrl}/users/register`, requestOptions).then(handleResponse);
+}
+
+function update(user) {
+    const requestOptions = {
+        method: 'PUT',
+        headers: { ...authHeader(), 'Content-Type': 'application/json' },
+        body: JSON.stringify(user)
+    };
+
+    return fetch(`${config.apiUrl}/users/${user.id}`, requestOptions).then(handleResponse);;
+}
+
+// prefixed function name with underscore because delete is a reserved word in javascript
+function _delete(id) {
+    const requestOptions = {
+        method: 'DELETE',
+        headers: authHeader()
+    };
+
+    return fetch(`${config.apiUrl}/users/${id}`, requestOptions).then(handleResponse);
+}
+
+function handleResponse(response) {
+    return response.text().then(text => {
+        const data = text && JSON.parse(text);
+        if (!response.ok) {
+            if (response.status === 401) {
+                // auto logout if 401 response returned from api
+                logout();
+                location.reload(true);
+            }
+
+            const error = (data && data.message) || response.statusText;
+            return Promise.reject(error);
+        } 
+        return data;
+    });
+}
+
+//set login handler for json db
+function handleResponseLogin(response,username,password) {
+    return response.text().then(text => {
+        const data = text && JSON.parse(text);
+        if (!response.ok) {
+            if (response.status === 401) {
+                // auto logout if 401 response returned from api
+                logout();
+                location.reload(true);
+            }
+
+            const error = (data && data.message) || response.statusText;
+            return Promise.reject(error);
+        }
+        if(data && data.length > 0){
+            
+            var correct = data.filter(d => d.username  == username && d.password == password);
+            if(correct && correct.length > 0){
+                return correct[0];
+            }else{
+                const error = 'Invalid credential';
+                return Promise.reject(error);
+            }
+
+            
+        }else{
+            const error = 'Invalid credential';
+            return Promise.reject(error);
+        }
+        
+    });
+}
